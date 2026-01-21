d9c59af68622e0ba8b4e32b5a3b95dffec366178
Joshua Dilley <jldilley@iu.edu>
2026-01-21 14:03:01 -0500

    Add files via upload
    A sample class primarily authored by Professor Abiona to be used with EmployeeTester. This is to help us get a feel for how classes work and are called by other programs.

---
+++ Employee.java
@@ -0,0 +1,29 @@
+/*Name: Joshua Dilley 
+Primary Author: Dr. Olatunde Abiona
+Date: 1/21/2026
+Compiler: Apache Net Beans IDE 24
+Project: Example Program Employee
+Action: Creates a class for Employee objects
+*/
+package c307;
+
+public class Employee 
+{
+    String Name;
+    int Age;
+    String Job;
+//---------------------------Constructor---------------------------------------    
+    public Employee (String EmpName, int EmpAge, String EmpJob)
+    {
+        Name = EmpName;
+        Age = EmpAge;
+        Job = EmpJob;
+    }        
+//---------------------------Methods-------------------------------------------
+    public void Greet()
+    {System.out.println("Good day, I am " + Name + ".");}
+    
+    public void Work()
+    {System.out.println(Name + " is working as a/an " + Job + ".");}        
+    
+}
